cf27d10ce195cf06bb99c59364c4f501af65616a
Francis Szakacs <szakacsszilard@gmail.com>
2024-04-23 11:59:58 -0400

    feat: settings to customize delays; feat: hotkey support

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
+
+
 let currentTypingSession = null;
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log('content.js: Message received:', request.action); // Log message received
+    //console.log("content.js: Message received:", request.action); // Log message received
 
     if (request.action === "emulateTyping") {
-        console.log("content.js: Action received: emulateTyping");
+        //console.log("content.js: Action received: emulateTyping");
+        let settings = request.settings;
         currentTypingSession = Math.random().toString(); // Create a new unique typing session identifier
-        navigator.clipboard
-            .readText()
-            .then((clipText) => {
-                console.log("content.js: Clipboard text read:", clipText); // Log clipboard text
-                emulateTyping(clipText, currentTypingSession, request.delayedStart);
-            });
+        navigator.clipboard.readText().then((clipText) => {
+            //console.log("content.js: Clipboard text read:", clipText); // Log clipboard text
+            emulateTyping(clipText, currentTypingSession, request.delayedStart, settings);
+        });
     } else if (request.action === "stopTyping") {
         currentTypingSession = null; // Invalidate the current typing session
     }
@@ -20,18 +21,18 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 // Listen for any keydown event at the window level
 window.addEventListener("keydown", function () {
     currentTypingSession = null; // Invalidate the current typing session
-    console.log('content.js: Keydown event detected'); // Log keydown event
+    //console.log("content.js: Keydown event detected"); // Log keydown event
 });
 
-function emulateTyping(text, session, delayedStart) {
+function emulateTyping(text, session, delayedStart, settings) {
     const activeElement = document.activeElement;
-    console.log('content.js: Active element:', activeElement); // Log the active element
+    //console.log("content.js: Active element:", activeElement); // Log the active element
 
     let i = 0;
 
     const startTyping = function () {
         function typeNextCharacter() {
-            console.log('content.js: Typing character', text[i]); // Log character being typed
+           // console.log("content.js: Typing character", text[i]); // Log character being typed
 
             if (i < text.length && session === currentTypingSession) {
                 // Create a KeyboardEvent instance
@@ -49,10 +50,14 @@ function emulateTyping(text, session, delayedStart) {
                 // Attempt to insert the text using document.execCommand
                 document.execCommand("insertText", false, text[i++]);
 
-                let delay = Math.random() * (200 - 50) + 50;
+                // Set the initial delay to a random value between MIN_DELAY and MAX_DELAY.
+                // This simulates the natural variation in the time it takes a human to press each key.
+                let delay = Math.random() * (settings.maxDelay - settings.minDelay) + settings.minDelay;
 
-                if (Math.random() < 0.05) {
-                    delay += Math.random() * (700 - 200) + 200;
+                // With a 5% chance, add an additional delay to the base delay.
+                // This simulates the occasional longer pauses a human might take while typing, such as when thinking or moving from one part of the keyboard to another.
+                if (Math.random() < settings.extraDelayChance) {
+                    delay += Math.random() * (settings.extraDelayMax - settings.extraDelayMin) + settings.extraDelayMin;
                 }
 
                 setTimeout(typeNextCharacter, delay);
@@ -63,7 +68,7 @@ function emulateTyping(text, session, delayedStart) {
     };
 
     if (delayedStart) {
-        console.log('content.js: Starting typing with a delay');
+        //console.log("content.js: Starting typing with a delay");
         setTimeout(startTyping, 0); // Delay is in milliseconds
     } else {
         startTyping();
--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 document.getElementById('startTyping').addEventListener('click', function() {
-  console.log("popup.js: Start typing button clicked");
+  //console.log("popup.js: Start typing button clicked");
 
   // Using a timeout function to delay the message being sent by 5 seconds
   setTimeout(function() {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           var currentTab = tabs[0]; // There should be only one in this array
-          console.log(`popup.js: Sending message to tab ${currentTab.id}`);
+          //console.log(`popup.js: Sending message to tab ${currentTab.id}`);
           chrome.tabs.sendMessage(currentTab.id, {
               action: 'emulateTyping',
               delayedStart: true,
           }, function(response) {
-              console.log("popup.js: Message sent: emulateTyping");
+              //console.log("popup.js: Message sent: emulateTyping");
           });
       });
   }, 5000); // 5000 milliseconds (5 seconds) delay
